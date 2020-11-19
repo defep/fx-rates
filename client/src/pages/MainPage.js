08f55554d1ac2a01d3e01ff0aa0fba9320892ac1
defep <federico.palma@hotmail.com>
2020-11-18 23:20:01 -0300

    Hide conversion form if there is no data

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 class MainPage extends Component {
 
     state = {
+        visible: false,
         amount: 1,
         conversion: 0,
         pair: '',
@@ -17,12 +18,15 @@ class MainPage extends Component {
         return fetch('/api/rates')
             .then(res => res.json())
             .then(data => {
-                this.setState({
-                    pair: data[0]._id,
-                    from: data[0]._id.substr(0,3),
-                    to: data[0]._id.substr(3),
-                    rates: data
-                })
+                if (data.length > 0) {
+                    this.setState({
+                        visible: true,
+                        pair: data[0]._id,
+                        from: data[0]._id.substr(0, 3),
+                        to: data[0]._id.substr(3),
+                        rates: data
+                    })
+                }
             })
             .catch(console.log)
     }
@@ -42,7 +46,7 @@ class MainPage extends Component {
         if (obj) {
             this.setState({
                 pair: obj._id,
-                from: obj._id.substr(0,3),
+                from: obj._id.substr(0, 3),
                 to: obj._id.substr(3),
                 conversion: parseFloat(obj.rate * this.state.amount) * (obj.fee / 100 + 1)
             });
@@ -56,19 +60,30 @@ class MainPage extends Component {
     }
 
     render() {
+        const { visible } = this.state;
         return (<div className="container">
             <div className="row">
-                <div className="col-md-12">
+                <div className="col">
                     <h1 className="mt-5">FX Rates</h1>
+                </div>
+            </div>
 
-                    <form>
+            <form style={{ display: (visible ? 'block' : 'none') }}>
+                <div className="d-flex justify-content-center">
+                    <div className="p-2">
+                        <label>Amount</label>
                         <input
+                            className="form-control"
                             name="amount"
                             type="number"
                             min="0"
                             value={this.state.amount}
                             onChange={this.handleChange} />
+                    </div>
+                    <div className="p-2">
+                        <label>Conversion</label>
                         <select
+                            className="form-control"
                             name="pair"
                             value={this.state.pair}
                             onChange={this.handleChange}>
@@ -76,12 +91,18 @@ class MainPage extends Component {
                                 <option key={e._id} value={e._id}>{e._id}</option>
                             ))}
                         </select>
-                    </form>
-
-                    <h5>{this.state.amount} {this.state.from} equals</h5>
-                    <h4>{this.state.conversion.toFixed(2)} {this.state.to}</h4>
-
-                    <Link to="/admin">Click here to create a new rate.</Link>
+                    </div>
+                </div>
+            </form>
+            <div className="row" style={{ display: (visible ? 'block' : 'none') }}>
+                <div className="col-md-12 text-center">
+                    <h5 className="mt-4">{this.state.amount} {this.state.from} equals to</h5>
+                    <h1 className="mt-0 mb-5">{this.state.conversion.toFixed(2)} {this.state.to}</h1>
+                </div>
+            </div>
+            <div className="row text-center">
+                <div className="col">
+                    <Link to="/admin">Click here to create a new rate</Link>
                 </div>
             </div>
         </div>);
