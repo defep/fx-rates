d895aa3c573deb5a79865adb7d9605ecdd9e75f8
defep <federico.palma@hotmail.com>
2020-11-27 10:03:18 -0300

    Replace function bind with arrow function

--- client/src/components/RateForm.js
+++ client/src/components/RateForm.js
@@ -22,12 +22,9 @@ class RateForm extends Component {
             rate: 0,
             pair: currencies[0].key
         };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleInputChange(event) {
+    handleInputChange = (event) => {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
